a0df262604e04cf84c368203458a02293783c22d
mchung03 <minhee.chung14@gmail.com>
2023-07-05 22:24:24 -0700

    completed basic html and css layout

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # challenge-4
+
+## Resources
+css-tricks: https://css-tricks.com/snippets/css/a-guide-to-flexbox/
+w3schools: https://www.w3schools.com/css/css3_buttons.asp
---
+++ assets/js/style.js
@@ -0,0 +1,14 @@
+// Button for start quiz
+// If statement for button clicked addEventListener
+
+// Switches to screen with first question
+// Switch case for every question?
+
+//If answer right, move on to next question
+//If answer wrong, subtract 5 seconds
+
+// Timer going
+// If timer reaches 0 or if all questions are answered, game over screen
+
+// High score screen
+// Enter initials and submit button
