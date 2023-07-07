36c985b7a39be26a8463d131db1ba671bf5a915a
mchung03 <minhee.chung14@gmail.com>
2023-07-06 20:57:52 -0700

    changed class and id names to match javascript question and added high score box

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 
 ## Resources
 css-tricks: https://css-tricks.com/snippets/css/a-guide-to-flexbox/
+
+stackoverflow: https://stackoverflow.com/questions/37252041/storing-quiz-questions-in-array-of-objects
+
 w3schools: https://www.w3schools.com/css/css3_buttons.asp
--- assets/js/style.js
+++ assets/js/style.js
@@ -1,20 +1,49 @@
-// Button for start quiz
+// Start quiz and timer when button is clicked
+// Switch start box screen to question box screen
 var startButton = document.querySelector("#start-button");
 var startBox = document.querySelector("#start-box");
-var questionBox = document.querySelector("#question-box")
+var questionBox = document.querySelector("#question-box");
 
 startButton.addEventListener("click", start)
 function start() {
-    console.log("Start button has been clicked");
     startBox.classList.add("hidden");
     questionBox.classList.remove("hidden");
     setTime();
+    showQuestion(questions);
 }
 
+var questions = [
+    {
+        question: "To make a folder in Git Bash, the command is: ",
+        choices: ["cd", "touch", "mkdir", "git mkdir"],
+        answer: 2
+    },
+    {
+        question: "Which of the following flexbox commands moves elements vertically?",
+        choices: ["justify-content", "flex-direction", "flex-direction", "align-items"],
+        answer: 3
+    },
+    {
+        question: "To call a function, you need",
+        choices: ["Just the name", "Parentheses", "Brackets", "Quotation Marks"],
+        answer: 1
+    },
+    {
+        question: 'What is the notation for "and"?',
+        choices: ["||", "+=", "&&", '"and"'],
+        answer: 2
+    },
+    {
+        question: "Which property combines indexes into one string?",
+        choices: [".join", ".concat", ".pop", ".add"],
+        answer: 0
+    }
+]
+
 // Switches to screen with first question
-// Switch case for every question?
-var question = [""]
-var questionCount;
+// Array for each question
+
+
 //If answer right, move on to next question
 //If answer wrong, subtract 5 seconds
 
@@ -43,12 +72,14 @@ function sendMessage() {
 
 // High score screen
 // Enter initials and submit button
-var name = "Minhee"
-var score = 80;
+// function highScore() {
+//     var name = prompt("What is your name?")
+//     var score = 80;
 
-var scoreStr = localStorage.getItem("scores") || "[]"
-var scores = JSON.parse(scoreStr)
-console.log(scores)
-scores.push({name:name, score:score})
-scoreStr = JSON.stringify(scores)
-localStorage.setItem("scores", scoreStr)
+//     var scoreStr = localStorage.getItem("scores") || "[]"
+//     var scores = JSON.parse(scoreStr)
+//     console.log(scores)
+//     scores.push({name:name, score:score})
+//     scoreStr = JSON.stringify(scores)
+//     localStorage.setItem("scores", scoreStr)
+// }
